7040ea8a1fb6ffd255ea7c8dfef11d439c8b940f
Ben <alphanumu@gmail.com>
2021-04-30 00:59:05 -0700

    Added SR2 (Switchres 2.0). library implimentation only (.so or .dll required)

--- gfx/video_crt_switch.c
+++ gfx/video_crt_switch.c
@@ -19,10 +19,21 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "../retroarch.h"
 #include "video_crt_switch.h"
 #include "video_display_server.h"
+
+//#ifdef __linux__
+#define LIBSWR "libswitchres.so"
+//#elif _WIN32
+//#define LIBSWR "libswitchres.dll"
+//#endif
+
+#include <switchres/switchres_wrapper.h>
+
+LIBTYPE dlp;
 
 #ifdef HAVE_CONFIG_H
 #include "../config.h"
@@ -76,6 +87,79 @@ static void switch_res_crt(
       videocrt_switch_t *p_switch,
       unsigned width, unsigned height)
 {
+   unsigned char interlace = 0,   ret;
+   const char* err_msg;
+   
+   //printf("About to open %s.\n", LIBSWR);
+
+	// Load the lib
+   dlp = OPENLIB(LIBSWR);
+
+	// Loading failed, inform and exit
+	if (!dlp) {
+		//printf("Loading %s failed.\n", LIBSWR);
+		//printf("Error: %s\n", LIBERROR());
+		
+	}
+	
+	//printf("Loading %s succeded.\n", LIBSWR);
+
+
+	// Load the init()
+	LIBERROR();
+	srAPI* SRobj =  (srAPI*)LIBFUNC(dlp, "srlib");
+	if ((err_msg = LIBERROR()) != NULL) {
+		//printf("Failed to load srAPI: %s\n", err_msg);
+		CLOSELIB(dlp);
+	
+	}
+
+   	// Testing the function
+	//printf("Init a new switchres_manager object:\n");
+	SRobj->init();
+	SRobj->sr_init_disp();
+
+	// Call mode + get result values
+	int w = width, h = height;
+	double rr = p_switch->ra_tmp_core_hz;
+   if (height >= 300)
+	   interlace = 1;
+   else
+      interlace = 0;
+
+
+	sr_mode srm;
+
+   //printf("Orignial resolution expected: %dx%d@%f-%d\n", w, h, rr, interlace);
+
+	ret = SRobj->sr_add_mode(w, h, rr, interlace, &srm);
+	if(!ret) 
+	{
+		//printf("ERROR: couldn't add the required mode. Exiting!\n");
+		SRobj->deinit();
+
+	}
+	//printf("Got resolution: %dx%d%c@%f\n", srm.width, srm.height, srm.interlace, srm.refresh);
+	//printf("Press Any Key to switch to new mode\n");
+
+
+   ret = SRobj->sr_switch_to_mode(srm.width, srm.height, rr, srm.interlace, &srm);
+	if(!ret) 
+	{
+		//printf("ERROR: couldn't switch to the required mode. Exiting!\n");
+		SRobj->deinit();
+
+	}
+	
+
+
+	// Clean the mess, kiss goodnight SR
+	SRobj->deinit();
+
+	// We're done, let's closer
+	CLOSELIB(dlp);
+
+   /*
    video_display_server_set_resolution(width, height,
          p_switch->ra_set_core_hz,
          p_switch->ra_core_hz,
@@ -92,6 +176,8 @@ static void switch_res_crt(
    crt_switch_driver_reinit();
 #endif
    video_driver_apply_state_changes();
+
+*/
 }
 
 /* Create correct aspect to fit video 
